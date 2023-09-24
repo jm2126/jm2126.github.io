e5dd6a60a645bf3ea0bcf30c574a40091e869053
jm2126 <145024317+jm2126@users.noreply.github.com>
2023-09-24 16:42:19 +0100

    Create currentDate.js

---
+++ script/currentDate.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to get the current date
+
+    //using the getDate
+    const d = new Date();
+    d.getDate(); //use getDate method to get current Date
+    console.log(`Date: ${d}.`)//display to user
+
+    //expected output - Date: day, date, time, time zone
