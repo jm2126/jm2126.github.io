f495a6c8c583f5318b02062fa125154198d1c77b
jm2126 <145024317+jm2126@users.noreply.github.com>
2023-09-24 17:04:04 +0100

    change format to dd/mm/yyyy
    Still working on getting 09 for the month instead of just 9

--- script/currentDate.js
+++ script/currentDate.js
@@ -1,8 +1,12 @@
 //Write a JavaScript program to get the current date
 
-    //using the getDate
-    const d = new Date();
-    d.getDate(); //use getDate method to get current Date
-    console.log(`Date: ${d}.`)//display to user
+    //declare variables
+    const date = new Date();//return current date and time
 
-    //expected output - Date: day, date, time, time zone
+    var day = date.getDate(); //use getDate method to get current day
+    var month = date.getMonth() + 1;//add 1 because getMonth() returns numbers from 0 to 11
+    var year = date.getFullYear();//display
+    
+    console.log(`Date: ${day} / ${month} / ${year}`);//display to user - figure out how to get 09
+
+    //expected output - Date: dd-mm-yyyy
