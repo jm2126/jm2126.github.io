0804401473df159cf013d4f3ceab4f8932f0d671
jm2126 <145024317+jm2126@users.noreply.github.com>
2023-09-24 17:09:19 +0100

    finished solution
    added if statement and used string concatenation to get dd/mm/yyyy format

--- script/currentDate.js
+++ script/currentDate.js
@@ -6,7 +6,13 @@
     var day = date.getDate(); //use getDate method to get current day
     var month = date.getMonth() + 1;//add 1 because getMonth() returns numbers from 0 to 11
     var year = date.getFullYear();//display
+
+    //if the month is less than 10 then add 0 in front of the month
+    if (month < 10){
+        //using string concatenation
+        month = "0" + month;
+    }
     
-    console.log(`Date: ${day} / ${month} / ${year}`);//display to user - figure out how to get 09
+    console.log(`Date: ${day} / ${month} / ${year}`);//display to user - fingure out how to get 09
 
     //expected output - Date: dd-mm-yyyy
