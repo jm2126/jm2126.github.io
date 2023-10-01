73c4cffa1dc1e6682a6235b63ba0c5cd67773612
jm2126 <145024317+jm2126@users.noreply.github.com>
2023-10-01 19:12:44 +0100

    Jm2126 patch 1 (#1)
    * Update currentDate.js - scope
    
    Put variables defined with var inside function - only available inside function.
    
    * Update currentDate.js - calling function
    
    Experimenting with calling a function using no name
    
    * Update currentDate.js - calling function
    
    Experimenting with calling a function with self invocation

--- lab exercises/script/currentDate.js
+++ lab exercises/script/currentDate.js
@@ -1,18 +1,17 @@
-//Write a JavaScript program to get the current date
+//Write a JavaScript program to get the current date 
 
+(function dateFunction(){
     //declare variables
-    const date = new Date();//return current date and time
+    const date = new Date();//returns current date and time
 
     var day = date.getDate(); //use getDate method to get current day
     var month = date.getMonth() + 1;//add 1 because getMonth() returns numbers from 0 to 11
-    var year = date.getFullYear();//display
+    var year = date.getFullYear();//using getFullYear to gte 2023
 
-    //if the month is less than 10 then add 0 in front of the month
+    //if month is less than 10 then add 0 in front of month
     if (month < 10){
         //using string concatenation
         month = "0" + month;
     }
-    
-    console.log(`Date: ${day} / ${month} / ${year}`);//display to user - fingure out how to get 09
-
-    //expected output - Date: dd-mm-yyyy
+    console.log(`Date: ${day} / ${month} / ${year}`);//expected output - Date: dd-mm-yyyy
+} ()); //call function with self invocation
