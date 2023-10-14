91bf40529c6c34b0dad6b5c7e666538d3590c5f6
jm2126 <145024317+jm2126@users.noreply.github.com>
2023-10-14 16:29:32 +0100

    Create todo.js - will be used to create + edit list
    may experiment with jquery

---
+++ Task Star Website/javascript/todo.js
@@ -0,0 +1,10 @@
+
+//create a function to create a new list
+function createList(){
+
+}
+
+//create a functions to add, edit and delete from list
+
+//Mark tasks as complete.
+//Mark tasks as priority.
